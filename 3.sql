28ac68d85300b00c80d8fea6d6051e9429c266ed
hngdh <407868@edu.itmo.ru>
2025-04-22 19:07:55 +0300

    ‎

--- 3.sql
+++ 3.sql
@@ -1,10 +1,8 @@
 -- psql -h pg -d ucheb
 
-select count(ИД) as result
-from (select Н_ЛЮДИ.ИД,
-(cast(cast(CURRENT_DATE - cast(Н_ЛЮДИ.ДАТА_РОЖДЕНИЯ as date) as int)/365 as int)) as age,
-Н_ФОРМЫ_ОБУЧЕНИЯ.ИМЯ_В_ИМИН_ПАДЕЖЕ as form
+select count(Н_ЛЮДИ.ИД)
 from ((Н_ЛЮДИ inner join Н_ОБУЧЕНИЯ on Н_ЛЮДИ.ИД = Н_ОБУЧЕНИЯ.ЧЛВК_ИД)
-inner join Н_ФОРМЫ_ОБУЧЕНИЯ on Н_ОБУЧЕНИЯ.ВИД_ОБУЧ_ИД = Н_ФОРМЫ_ОБУЧЕНИЯ.ИД))
-where form = 'вечерняя' and age > 25
+inner join Н_ФОРМЫ_ОБУЧЕНИЯ on Н_ОБУЧЕНИЯ.ВИД_ОБУЧ_ИД = Н_ФОРМЫ_ОБУЧЕНИЯ.ИД)
+where Н_ФОРМЫ_ОБУЧЕНИЯ.ИМЯ_В_ИМИН_ПАДЕЖЕ = 'вечерняя'
+and extract(year from age(CURRENT_DATE, Н_ЛЮДИ.ДАТА_РОЖДЕНИЯ)) > 25
 order by 1;
--- 5.sql
+++ 5.sql
@@ -1,10 +1,11 @@
 -- select (cast(cast(CURRENT_DATE - cast(Н_ЛЮДИ.ДАТА_РОЖДЕНИЯ as date) as int)/365 as int))
 -- from (Н_УЧЕНИКИ inner join Н_ЛЮДИ on Н_УЧЕНИКИ.ЧЛВК_ИД = Н_ЛЮДИ.ИД) where Н_УЧЕНИКИ.ГРУППА = '1101';
 
-select * from (with stud_list as
-(select cast(Н_УЧЕНИКИ.ГРУППА as int) as group_num,
-cast(cast(CURRENT_DATE - cast(Н_ЛЮДИ.ДАТА_РОЖДЕНИЯ as date) as int)/365 as int) as age
-from (Н_УЧЕНИКИ inner join Н_ЛЮДИ on Н_УЧЕНИКИ.ЧЛВК_ИД = Н_ЛЮДИ.ИД))
-select group_num, cast(avg(age) as int) as avg_age, (select min(age) from stud_list where group_num = '1101') as min_age from stud_list
-group by 1 order by 1)
-where avg_age > min_age;
+select Н_УЧЕНИКИ.ГРУППА,
+cast(avg(extract(year from age(CURRENT_DATE, Н_ЛЮДИ.ДАТА_РОЖДЕНИЯ))) as int),
+from Н_УЧЕНИКИ inner join Н_ЛЮДИ on Н_УЧЕНИКИ.ЧЛВК_ИД = Н_ЛЮДИ.ИД
+group by 1
+having avg(extract(year from age(CURRENT_DATE, Н_ЛЮДИ.ДАТА_РОЖДЕНИЯ))) >
+(select min(extract(year from age(CURRENT_DATE, Н_ЛЮДИ.ДАТА_РОЖДЕНИЯ)))
+from Н_УЧЕНИКИ inner join Н_ЛЮДИ on Н_УЧЕНИКИ.ЧЛВК_ИД = Н_ЛЮДИ.ИД
+where Н_УЧЕНИКИ.ГРУППА = '1101');
